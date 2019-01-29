0413b2ef91baa40c90aba33816b430bb916931d0
dylan <dylan@youban.org>
2019-01-29 17:54:46 +0800

    优化滚动穿透

--- src/components/push-component/push-component.js
+++ src/components/push-component/push-component.js
@@ -10,23 +10,41 @@ export default class AdPush extends Component {
     }
 
     render() {
-        return (
-            <div>
-                {this.state.showAd && this.props.couponSent!=undefined&&!this.props.newUser ? (
-                    <div className="coupons" onClick={()=>{this.setState({showAd:false})}}>
-                        <div className="content">
-                            <h3>嗨！{this.props.newUser?"新朋友":"老朋友"}</h3>
-                            <div className="desc">
-                                <span className="shuli">送你</span>
-                                <span>{this.props.couponSent.couponMoney}</span>
-                                <span>元</span>
-                            </div>
-                            <div className="title">{this.props.couponSent.name}</div>
+        if(1==1){
+            return(
+                <div>
+                    {this.state.showAd && this.props.couponSent != undefined && !this.props.newUser ? (
+                        <div className="coupons" onClick={() => {
+                            this.setState({showAd: false})
+                        }}>
+                            <img className="festival" src="//udata.youban.com/webimg/wxyx/push/festival.png" alt=""/>
+                            <div className="festival-close" onClick={() => {
+                                this.setState({showAd: false})
+                            }}></div>
                         </div>
-                        <div class="close-coupon" onClick={()=>{this.setState({showAd:false})}}></div>
-                    </div>
-                ) : null}
-            </div>
-        )
+                    ) : null}
+                </div>
+            )
+        }else{
+            return (
+                <div>
+                    {this.state.showAd && this.props.couponSent!=undefined&&!this.props.newUser ? (
+                        <div className="coupons" onClick={()=>{this.setState({showAd:false})}}>
+                            <div className="content">
+                                <h3>嗨！{this.props.newUser?"新朋友":"老朋友"}</h3>
+                                <div className="desc">
+                                    <span className="shuli">送你</span>
+                                    <span>{this.props.couponSent.couponMoney}</span>
+                                    <span>元</span>
+                                </div>
+                                <div className="title">{this.props.couponSent.name}</div>
+                            </div>
+                            <div class="close-coupon" onClick={()=>{this.setState({showAd:false})}}></div>
+                        </div>
+                    ) : null}
+                </div>
+            )
+        }
+
     }
 }
